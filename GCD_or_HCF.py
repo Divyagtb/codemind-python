6ed8d7333afc391537c4ddba12becca3b96fe83c
Divyagtb <84368755+Divyagtb@users.noreply.github.com>
2022-03-28 20:01:21 +0530

    num1,num2=map(int,input().split()) a=num1 b=num2 if (num1==0):     print(a,b,b) while(num2!=0):     if(num1>num2):         num1=num1-num2     else:         num2=num2-num1 hcf=num1 print(hcf)

---
+++ GCD_or_HCF.py
@@ -0,0 +1,15 @@
+num1,num2=map(int,input().split())
+a=num1
+b=num2
+if (num1==0):
+    print(a,b,b)
+while(num2!=0):
+    if(num1>num2):
+        num1=num1-num2
+    else:
+        num2=num2-num1
+hcf=num1
+print(hcf)
+    
+    
+    
